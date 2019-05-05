ba069a4682a3d15414fd844a884bee5b9bca8b5b
Alex Livingstone <livingstone.alex@gmail.com>
2019-05-06 00:26:02 +1000

    use trakt api for show searching etc

--- tvtrend.py
+++ tvtrend.py
@@ -4,4 +4,6 @@ import os
 
 app = Flask(__name__)
 
+app.config.from_pyfile('config.py')
+
 import views
--- views.py
+++ views.py
@@ -1,11 +1,15 @@
 from flask import render_template, request, redirect, url_for, jsonify
 from tvtrend import app
-import tvdb_api, requests
+import requests, json
 from bs4 import BeautifulSoup
 import numpy as np
 from datetime import datetime
 
-t = tvdb_api.Tvdb()
+headers = {
+    'Content-Type': 'application/json',
+    'trakt-api-version': '2',
+    'trakt-api-key': app.config['TRAKT_API']
+}
 
 @app.route('/')
 def home():
@@ -14,31 +18,32 @@ def home():
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
-        showname = request.form['showname']
+        showname = request.form['showname'].replace(' ','%20')
 
-        show = t[showname]
+        r = requests.get('https://api.trakt.tv/search/show?query={}'.format(showname),headers=headers)
+        imdbId = json.loads(r.content)[0]['show']['ids']['imdb']
 
-        return redirect(url_for('plot_show',tvdbId=show.data['id'])+'?source={}'.format(request.form['source']))
+        return redirect(url_for('plot_show',imdbId=imdbId)+'?source={}'.format(request.form['source']))
     else:
         return redirect(url_for('home'))
 
-@app.route('/<string:tvdbId>')
-def plot_show(tvdbId):
-    show = t[int(tvdbId)]
-
+@app.route('/<string:imdbId>')
+def plot_show(imdbId):
     if 'source' in request.args.keys():
         source = request.args['source']
     else:
         source = 'imdb'
 
+    r = requests.get('https://api.trakt.tv/shows/{}/seasons'.format(imdbId),headers=headers)
+
     ratings = {}
     epInfo = {'names': [], 'dates': []}
 
-    for season in show.keys():
+    for season in [a['number'] for a in json.loads(r.content)]:
         if season > 0:
             seasonratings = []
             if source =='imdb':
-                r = requests.get('https://www.imdb.com/title/{}/episodes?season={}'.format(show.data['imdbId'],season))
+                r = requests.get('https://www.imdb.com/title/{}/episodes?season={}'.format(imdbId,season))
                 soup = BeautifulSoup(r.content)
                 for div in soup.find_all("div","ipl-rating-star small"):
                     span = div.find_all("span","ipl-rating-star__rating")[0]
@@ -49,7 +54,7 @@ def plot_show(tvdbId):
                     epInfo['dates'].extend([a.text.strip() for a in eplist.find_all("div","airdate")])
                     ratings[season] = seasonratings
             elif source == 'trakt':
-                r = requests.get('https://trakt.tv/shows/{}/seasons/{}'.format(show.data['imdbId'],season))
+                r = requests.get('https://trakt.tv/shows/{}/seasons/{}'.format(imdbId,season))
                 soup = BeautifulSoup(r.content)
                 rates = [int(a.text[:-1])/10 for a in soup.find_all("div", "percentage")]
                 names = [a.text for a in soup.find_all("span","main-title")[::2]]
@@ -77,7 +82,10 @@ def plot_show(tvdbId):
     xx = list(np.arange(1,len(seriesdata)+1))
     fits['series'] = [xx,list(np.poly1d(np.polyfit(xx, seriesdata, 1))(np.unique(xx)))]
 
-    return render_template('plot.html',show=show,ratings=data,epInfo=epInfo,fits=fits)
+    r = requests.get('https://api.trakt.tv/shows/{}'.format(imdbId),headers=headers)
+    showname = json.loads(r.content)['title']
+
+    return render_template('plot.html',showname=showname,ratings=data,epInfo=epInfo,fits=fits)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
